340c73e3bf7849d34ab7bc80823faabf20d98720
CarlosCamposs <carlosdanct7@gmail.com>
2022-10-20 20:23:28 -0500

    data

---
+++ Proyecto 2/R/Modelo2.R
@@ -0,0 +1,29 @@
+# ////////////////////////////////////////////////////////////////////
+# Proyecto 2 - Equipo 9
+# Administración Integral de Riesgos
+
+# Cargamos bibliotecas
+
+library(knitr)
+library(xtable)
+library(printr)
+library(stargazer)
+library(rmarkdown)
+library(effects)
+library(car)
+library(AER)
+library(broom)
+library(stats)
+library(lmtest)
+library(sandwich)
+library(tinytex)
+library(openxlsx)
+library(readxl)
+library(nlWaldTest)
+library(forecast)
+library(tables)
+library(haven)
+library(nnet)
+library(MCMCpack)
+library(sampleSelection)
+library(corrplot)
