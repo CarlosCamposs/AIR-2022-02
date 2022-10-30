dd2718920f653b6f4f8b66734bcce934252445d8
CarlosCamposs <carlosdanct7@gmail.com>
2022-10-30 13:05:53 -0600

    VaR no paramétrico (una emisora)

--- Proyecto 3/Proyecto 3 (Equipo 9).R
+++ Proyecto 3/Proyecto 3 (Equipo 9).R
@@ -5,8 +5,218 @@ library(TTR)
 library(tidyquant)
 library(quantmod)
 
-
 cartera = c("BAC")
-getSymbols(cartera,src = "yahoo",from="2019-01-01",to="2022-09-01")
+getSymbols(cartera,src = "yahoo",from="2018-01-01",to="2022-09-01")
 head(BAC,n=3)
+
+
+precios<-BAC$BAC.Close
+precios
+
+# Rendimientos
+rendimientos<-diff(precios)/precios
+rendimientos<-rendimientos[c(-1),]
+
+# Ultimo precio
+ultimo_precio<-precios[length(precios),]
+  # Convertimos a vector
+ultimo_precio<-as.vector(ultimo_precio)
+  # Revaluación
+revaluacion<-ultimo_precio*(1+rendimientos)
+
+
+# P&L
+  PL<-ultimo_precio-revaluacion
+
+# //////////////////////////////////////////////////////////////////////////////////  
+# //////////////////////////////////////////////////////////////////////////////////  
+# Simulacion Historica
+  
+# Niveles de confianza
+alpha<-c(0.95,0.975,0.99)
+
+# VaR no parametrico (1 dia)
+VaR_1<-quantile(PL,probs=alpha)
+
+# VaR no parametrico (30 dia)
+VaR_30<-VaR_1*30
+
+# VaR no parametrico (180 dia)
+VaR_180<-VaR_1*180
+
+# VaR no parametrico (360 dia)
+VaR_360<-VaR_1*360
+
+
+# //////////////////////////////////////////////////////////////////////////////////  
+# //////////////////////////////////////////////////////////////////////////////////  
+# Montecarlo
+
+mean<-mean(rendimientos)  
+sd<-sd(rendimientos)  
+
+VaRSM95<-vector()  
+VaRSM975<-vector()  
+VaRSM99<-vector()  
+
+for(i in 1:5000){
+
+  # Simulacion de rendimientos (normal)
+  rendimiento_simulado<-rnorm(length(rendimientos),mean = mean,sd = sd)
+  
+  # Revaluacion
+  revaluacionSM<-ultimo_precio*(1+rendimiento_simulado)
+  
+  # P&L
+  PL_SM<-ultimo_precio-revaluacionSM
+  
+  # VaR  
+  VaR_SM95<-quantile(PL_SM,0.95)
+  VaR_SM975<-quantile(PL_SM,0.975)
+  VaR_SM99<-quantile(PL_SM,0.99)
+  
+VaRSM95[i]<-VaR_SM95  
+VaRSM975[i]<-VaR_SM975  
+VaRSM99[i]<-VaR_SM99  
+
+}
+
+# VaR Montecarlo (1 dia)
+VaRMontecarlo95<-mean(VaRSM95)
+VaRMontecarlo975<-mean(VaRSM975)
+VaRMontecarlo99<-mean(VaRSM99)
+
+VaR_1<-cbind(VaRMontecarlo95,VaRMontecarlo975,VaRMontecarlo99)
+colnames(VaR_1)<-c("95%","97.5%","99%")
+VaR_1
+
+# VaR Montecarlo (30 dia)
+VaR_30<-VaR_1*30
+
+# VaR Montecarlo (180 dia)
+VaR_180<-VaR_1*180
+
+# VaR Montecarlo (360 dia)
+VaR_360<-VaR_1*360
+
+
+# //////////////////////////////////////////////////////////////////////////////////  
+# //////////////////////////////////////////////////////////////////////////////////  
+# Bootstrapping
+
+# Funcion de P&L
+PL<-ultimo_precio-revaluacion
+
+
+VaRB95<-vector()  
+VaRB975<-vector()  
+VaRB99<-vector()  
+
+
+for(i in 1:5000){
+  
+  # Sample con replace=T
+  remuestreoBoots<-sample(PL, size=length(PL), replace = TRUE)
+
+  # VaR  
+  VaR_B95<-quantile(remuestreoBoots,0.95)
+  VaR_B975<-quantile(remuestreoBoots,0.975)
+  VaR_B99<-quantile(remuestreoBoots,0.99)
+  
+  VaRB95[i]<-VaR_B95  
+  VaRB975[i]<-VaR_B975  
+  VaRB99[i]<-VaR_B99  
+}
+
+
+# VaR Boostrapping (1 dia)
+VaRBoots95<-mean(VaRB95)
+VaRBoots975<-mean(VaRB975)
+VaRBoots99<-mean(VaRB99)
+
+VaR_1<-cbind(VaRBoots95,VaRBoots975,VaRBoots99)
+colnames(VaR_1)<-c("95%","97.5%","99%")
+VaR_1
+
+# VaR Boostrapping (30 dia)
+VaR_30<-VaR_1*30
+
+# VaR Boostrapping (180 dia)
+VaR_180<-VaR_1*180
+
+# VaR Boostrapping (360 dia)
+VaR_360<-VaR_1*360
+
+# //////////////////////////////////////////////////////////////////////////////////  
+# //////////////////////////////////////////////////////////////////////////////////  
+# Alisado Exponencial
+
+a<-0.95
+b<-0.05
+PL<-ultimo_precio-revaluacion
+
+# Indicador
+j<-length(PL)-1
+
+Alisado<-vector()
+
+for(i in 0:j){
+
+  Alisado[length(PL)-i]<-a^(i)*b
+    
+}
+
+
+Tabla_Alisado<-cbind(PL,Alisado)
+colnames(Tabla_Alisado)<-c("PL","Alisado")
+head(Tabla_Alisado)
+
+
+Tabla_Alisado<-as.data.frame(Tabla_Alisado)
+
+# Ordenamos tabla por P&L
+Tabla_Alisado <- Tabla_Alisado[with(Tabla_Alisado, order(-Tabla_Alisado$PL)), ] # Orden directo 
+head(Tabla_Alisado)
+
+# Which (pendiente)
+
+Alisado2<-Tabla_Alisado$Alisado
+head(Alisado2)
+
+Fx<-vector()
+for(i in 1:length(Alisado2)){
+  Fx[i]<-sum(Alisado2[i:length(Alisado2)])
+}
+head(Fx,15)
+
+
+Tabla_Alisado<-cbind(Tabla_Alisado,Fx)
+head(Tabla_Alisado)
+
+
+
+# VaR al 95% (1 día)
+tabla1<-Tabla_Alisado[which(Tabla_Alisado$Fx>=0.95),]
+VaRAE_95<-tabla1[length(tabla1$PL),"PL"]
+
+# VaR al 975% (1 día)
+tabla2<-Tabla_Alisado[which(Tabla_Alisado$Fx>=0.975),]
+VaRAE_975<-tabla1[length(tabla2$PL),"PL"]
+
+# VaR al 99% (1 día)
+tabla3<-Tabla_Alisado[which(Tabla_Alisado$Fx>=0.99),]
+VaRAE_99<-tabla1[length(tabla3$PL),"PL"]
+
+
+VaR_AlisadoExp<-cbind(VaRAE_95,VaRAE_975,VaRAE_99) 
+colnames(VaR_AlisadoExp) <-c("95%","97.5%","99%")
+VaR_AlisadoExp
+
+
+
+
+
+
+
+
 
