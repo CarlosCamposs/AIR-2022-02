c5ffadb344c24f1cd7628b394bd8b41e01c1d9b0
CarlosCamposs <carlosdanct7@gmail.com>
2022-11-06 00:22:24 -0600

    VaR no diversificado

--- Proyecto 3/Correcion.R
+++ Proyecto 3/Correcion.R
@@ -360,9 +360,11 @@ View(PL_Portafolio)
   #  mydata <- write.xlsx(Tabla_Alisado,".xlsx")
   #  saveWorkbook(mydata, file = "C:/Users/Carlos Campos/Desktop/GitHub/AIR-2022-02/Proyecto 3/Tabla_Alisado.xlsx", overwrite = TRUE)
   
-
+  
+#################################################################################################################
 # Creamos una tabla para cada emisora donde se ordenen los valores de su P&L
-
+# Las siguientes lineas (hasta la 748) solo calculan el VaR por Alisado exponencial de cada emisora
+  
 # ---------------------------------------------------------------------------------
   # WALMEX
   Tabla_Alisado1<-Tabla_Alisado[,c(1,11)]
@@ -440,3 +442,353 @@ View(PL_Portafolio)
   VaRAE_emisora2
   
 # ---------------------------------------------------------------------------------    
+
+# ---------------------------------------------------------------------------------
+  # GFNORTEO
+  Tabla_Alisado3<-Tabla_Alisado[,c(3,11)]
+  Tabla_Alisado3 <- Tabla_Alisado3[with(Tabla_Alisado3, order(-Tabla_Alisado3$GFNORTEO.MX)), ] 
+  
+  # Creamos la columna de Fx
+  Alisado3<-Tabla_Alisado3$Alisado
+  
+  Fx<-vector()
+  
+  for(i in 1:length(Alisado3)){
+    Fx[i]<-sum(Alisado3[i:length(Alisado3)])
+  }
+  
+  head(Fx,30)
+  
+  # Unimos la columna de Fx con la Tabla_Alisado
+  Tabla_Alisado3<-cbind(Tabla_Alisado3,Fx)
+  
+  # VaR al 95% (1 día)
+  tabla_emisora3_95<-Tabla_Alisado3[which(Tabla_Alisado3$Fx>=0.95),] # Guarda todos los valores mayores a 0.95
+  VaRAE_95EMISORA3<-tabla_emisora3_95[length(tabla_emisora3_95$Fx),1] # Se toma el primer valor que sea mayor a 0.95
+  
+  # VaR al 975% (1 día)
+  tabla_emisora3_975<-Tabla_Alisado3[which(Tabla_Alisado3$Fx>=0.975),]
+  VaRAE_975EMISORA3<-tabla_emisora3_975[length(tabla_emisora3_975$Fx),1]
+  
+  # VaR al 99% (1 día)
+  tabla_emisora3_99<-Tabla_Alisado3[which(Tabla_Alisado3$Fx>=0.99),]
+  VaRAE_99EMISORA3<-tabla_emisora3_99[length(tabla_emisora3_99$Fx),1]
+  
+  # Resultados - VaR 1 dia de WalMex  
+  VaRAE_emisora3<-cbind(VaRAE_95EMISORA3,VaRAE_975EMISORA3,VaRAE_99EMISORA3) 
+  colnames(VaRAE_emisora3) <-c("95%","97.5%","99%")  
+  VaRAE_emisora3
+  
+# ---------------------------------------------------------------------------------      
+  
+# ---------------------------------------------------------------------------------
+  # GMEXICOB
+  Tabla_Alisado4<-Tabla_Alisado[,c(4,11)]
+  Tabla_Alisado4 <- Tabla_Alisado4[with(Tabla_Alisado4, order(-Tabla_Alisado4$GMEXICOB.MX)), ] 
+  
+  # Creamos la columna de Fx
+  Alisado4<-Tabla_Alisado4$Alisado
+  
+  Fx<-vector()
+  
+  for(i in 1:length(Alisado4)){
+    Fx[i]<-sum(Alisado4[i:length(Alisado4)])
+  }
+  
+  head(Fx,30)
+  
+  # Unimos la columna de Fx con la Tabla_Alisado
+  Tabla_Alisado4<-cbind(Tabla_Alisado4,Fx)
+  
+  # VaR al 95% (1 día)
+  tabla_emisora4_95<-Tabla_Alisado4[which(Tabla_Alisado4$Fx>=0.95),] # Guarda todos los valores mayores a 0.95
+  VaRAE_95EMISORA4<-tabla_emisora4_95[length(tabla_emisora4_95$Fx),1] # Se toma el primer valor que sea mayor a 0.95
+  
+  # VaR al 975% (1 día)
+  tabla_emisora4_975<-Tabla_Alisado4[which(Tabla_Alisado4$Fx>=0.975),]
+  VaRAE_975EMISORA4<-tabla_emisora4_975[length(tabla_emisora4_975$Fx),1]
+  
+  # VaR al 99% (1 día)
+  tabla_emisora4_99<-Tabla_Alisado4[which(Tabla_Alisado4$Fx>=0.99),]
+  VaRAE_99EMISORA4<-tabla_emisora4_99[length(tabla_emisora4_99$Fx),1]
+  
+  # Resultados - VaR 1 dia de WalMex  
+  VaRAE_emisora4<-cbind(VaRAE_95EMISORA4,VaRAE_975EMISORA4,VaRAE_99EMISORA4) 
+  colnames(VaRAE_emisora4) <-c("95%","97.5%","99%")  
+  VaRAE_emisora4
+  
+# ---------------------------------------------------------------------------------      
+  
+# ---------------------------------------------------------------------------------
+  # TLEVISACPO
+  Tabla_Alisado5<-Tabla_Alisado[,c(5,11)]
+  Tabla_Alisado5 <- Tabla_Alisado5[with(Tabla_Alisado5, order(-Tabla_Alisado5$TLEVISACPO.MX)), ] 
+  
+  # Creamos la columna de Fx
+  Alisado5<-Tabla_Alisado5$Alisado
+  
+  Fx<-vector()
+  
+  for(i in 1:length(Alisado5)){
+    Fx[i]<-sum(Alisado5[i:length(Alisado5)])
+  }
+  
+  head(Fx,30)
+  
+  # Unimos la columna de Fx con la Tabla_Alisado
+  Tabla_Alisado5<-cbind(Tabla_Alisado5,Fx)
+  
+  # VaR al 95% (1 día)
+  tabla_emisora5_95<-Tabla_Alisado5[which(Tabla_Alisado5$Fx>=0.95),] # Guarda todos los valores mayores a 0.95
+  VaRAE_95EMISORA5<-tabla_emisora5_95[length(tabla_emisora5_95$Fx),1] # Se toma el primer valor que sea mayor a 0.95
+  
+  # VaR al 975% (1 día)
+  tabla_emisora5_975<-Tabla_Alisado5[which(Tabla_Alisado5$Fx>=0.975),]
+  VaRAE_975EMISORA5<-tabla_emisora5_975[length(tabla_emisora5_975$Fx),1]
+  
+  # VaR al 99% (1 día)
+  tabla_emisora5_99<-Tabla_Alisado5[which(Tabla_Alisado5$Fx>=0.99),]
+  VaRAE_99EMISORA5<-tabla_emisora5_99[length(tabla_emisora5_99$Fx),1]
+  
+  # Resultados - VaR 1 dia de WalMex  
+  VaRAE_emisora5<-cbind(VaRAE_95EMISORA5,VaRAE_975EMISORA5,VaRAE_99EMISORA5) 
+  colnames(VaRAE_emisora5) <-c("95%","97.5%","99%")  
+  VaRAE_emisora5
+  
+# ---------------------------------------------------------------------------------      
+  
+# ---------------------------------------------------------------------------------
+  # KIMBERA
+  Tabla_Alisado6<-Tabla_Alisado[,c(6,11)]
+  Tabla_Alisado6 <- Tabla_Alisado6[with(Tabla_Alisado6, order(-Tabla_Alisado6$KIMBERA.MX)), ] 
+  
+  # Creamos la columna de Fx
+  Alisado6<-Tabla_Alisado6$Alisado
+  
+  Fx<-vector()
+  
+  for(i in 1:length(Alisado6)){
+    Fx[i]<-sum(Alisado6[i:length(Alisado6)])
+  }
+  
+  head(Fx,30)
+  
+  # Unimos la columna de Fx con la Tabla_Alisado
+  Tabla_Alisado6<-cbind(Tabla_Alisado6,Fx)
+  
+  # VaR al 95% (1 día)
+  tabla_emisora6_95<-Tabla_Alisado6[which(Tabla_Alisado6$Fx>=0.95),] # Guarda todos los valores mayores a 0.95
+  VaRAE_95EMISORA6<-tabla_emisora6_95[length(tabla_emisora6_95$Fx),1] # Se toma el primer valor que sea mayor a 0.95
+  
+  # VaR al 975% (1 día)
+  tabla_emisora6_975<-Tabla_Alisado6[which(Tabla_Alisado6$Fx>=0.975),]
+  VaRAE_975EMISORA6<-tabla_emisora6_975[length(tabla_emisora6_975$Fx),1]
+  
+  # VaR al 99% (1 día)
+  tabla_emisora6_99<-Tabla_Alisado6[which(Tabla_Alisado6$Fx>=0.99),]
+  VaRAE_99EMISORA6<-tabla_emisora6_99[length(tabla_emisora6_99$Fx),1]
+  
+  # Resultados - VaR 1 dia de WalMex  
+  VaRAE_emisora6<-cbind(VaRAE_95EMISORA6,VaRAE_975EMISORA6,VaRAE_99EMISORA6) 
+  colnames(VaRAE_emisora6) <-c("95%","97.5%","99%")  
+  VaRAE_emisora6
+  
+# ---------------------------------------------------------------------------------      
+  
+# ---------------------------------------------------------------------------------
+  # GCARSOA1
+  Tabla_Alisado7<-Tabla_Alisado[,c(7,11)]
+  Tabla_Alisado7 <- Tabla_Alisado7[with(Tabla_Alisado7, order(-Tabla_Alisado7$GCARSOA1.MX)), ] 
+  
+  # Creamos la columna de Fx
+  Alisado7<-Tabla_Alisado7$Alisado
+  
+  Fx<-vector()
+  
+  for(i in 1:length(Alisado7)){
+    Fx[i]<-sum(Alisado7[i:length(Alisado7)])
+  }
+  
+  head(Fx,30)
+  
+  # Unimos la columna de Fx con la Tabla_Alisado
+  Tabla_Alisado7<-cbind(Tabla_Alisado7,Fx)
+  
+  # VaR al 95% (1 día)
+  tabla_emisora7_95<-Tabla_Alisado7[which(Tabla_Alisado7$Fx>=0.95),] # Guarda todos los valores mayores a 0.95
+  VaRAE_95EMISORA7<-tabla_emisora7_95[length(tabla_emisora7_95$Fx),1] # Se toma el primer valor que sea mayor a 0.95
+  
+  # VaR al 975% (1 día)
+  tabla_emisora7_975<-Tabla_Alisado7[which(Tabla_Alisado7$Fx>=0.975),]
+  VaRAE_975EMISORA7<-tabla_emisora7_975[length(tabla_emisora7_975$Fx),1]
+  
+  # VaR al 99% (1 día)
+  tabla_emisora7_99<-Tabla_Alisado7[which(Tabla_Alisado7$Fx>=0.99),]
+  VaRAE_99EMISORA7<-tabla_emisora7_99[length(tabla_emisora7_99$Fx),1]
+  
+  # Resultados - VaR 1 dia de WalMex  
+  VaRAE_emisora7<-cbind(VaRAE_95EMISORA7,VaRAE_975EMISORA7,VaRAE_99EMISORA7) 
+  colnames(VaRAE_emisora7) <-c("95%","97.5%","99%")  
+  VaRAE_emisora7
+  
+# ---------------------------------------------------------------------------------      
+  
+# ---------------------------------------------------------------------------------
+  # GAPB
+  Tabla_Alisado8<-Tabla_Alisado[,c(8,11)]
+  Tabla_Alisado8 <- Tabla_Alisado8[with(Tabla_Alisado8, order(-Tabla_Alisado8$GAPB.MX)), ] 
+  
+  # Creamos la columna de Fx
+  Alisado8<-Tabla_Alisado8$Alisado
+  
+  Fx<-vector()
+  
+  for(i in 1:length(Alisado8)){
+    Fx[i]<-sum(Alisado8[i:length(Alisado8)])
+  }
+  
+  head(Fx,30)
+  
+  # Unimos la columna de Fx con la Tabla_Alisado
+  Tabla_Alisado8<-cbind(Tabla_Alisado8,Fx)
+  
+  # VaR al 95% (1 día)
+  tabla_emisora8_95<-Tabla_Alisado8[which(Tabla_Alisado8$Fx>=0.95),] # Guarda todos los valores mayores a 0.95
+  VaRAE_95EMISORA8<-tabla_emisora8_95[length(tabla_emisora8_95$Fx),1] # Se toma el primer valor que sea mayor a 0.95
+  
+  # VaR al 975% (1 día)
+  tabla_emisora8_975<-Tabla_Alisado8[which(Tabla_Alisado8$Fx>=0.975),]
+  VaRAE_975EMISORA8<-tabla_emisora8_975[length(tabla_emisora8_975$Fx),1]
+  
+  # VaR al 99% (1 día)
+  tabla_emisora8_99<-Tabla_Alisado8[which(Tabla_Alisado8$Fx>=0.99),]
+  VaRAE_99EMISORA8<-tabla_emisora8_99[length(tabla_emisora8_99$Fx),1]
+  
+  # Resultados - VaR 1 dia de WalMex  
+  VaRAE_emisora8<-cbind(VaRAE_95EMISORA8,VaRAE_975EMISORA8,VaRAE_99EMISORA8) 
+  colnames(VaRAE_emisora8) <-c("95%","97.5%","99%")  
+  VaRAE_emisora8
+  
+# ---------------------------------------------------------------------------------      
+  
+# ---------------------------------------------------------------------------------
+  # PE&OLES
+  Tabla_Alisado9<-Tabla_Alisado[,c(9,11)]
+  Tabla_Alisado9 <- Tabla_Alisado9[with(Tabla_Alisado9, order(-Tabla_Alisado9$`PE&OLES.MX`)), ] 
+  
+  # Creamos la columna de Fx
+  Alisado9<-Tabla_Alisado9$Alisado
+  
+  Fx<-vector()
+  
+  for(i in 1:length(Alisado9)){
+    Fx[i]<-sum(Alisado9[i:length(Alisado9)])
+  }
+  
+  head(Fx,30)
+  
+  # Unimos la columna de Fx con la Tabla_Alisado
+  Tabla_Alisado9<-cbind(Tabla_Alisado9,Fx)
+  
+  # VaR al 95% (1 día)
+  tabla_emisora9_95<-Tabla_Alisado9[which(Tabla_Alisado9$Fx>=0.95),] # Guarda todos los valores mayores a 0.95
+  VaRAE_95EMISORA9<-tabla_emisora9_95[length(tabla_emisora9_95$Fx),1] # Se toma el primer valor que sea mayor a 0.95
+  
+  # VaR al 975% (1 día)
+  tabla_emisora9_975<-Tabla_Alisado9[which(Tabla_Alisado9$Fx>=0.975),]
+  VaRAE_975EMISORA9<-tabla_emisora9_975[length(tabla_emisora9_975$Fx),1]
+  
+  # VaR al 99% (1 día)
+  tabla_emisora9_99<-Tabla_Alisado9[which(Tabla_Alisado9$Fx>=0.99),]
+  VaRAE_99EMISORA9<-tabla_emisora9_99[length(tabla_emisora9_99$Fx),1]
+  
+  # Resultados - VaR 1 dia de WalMex  
+  VaRAE_emisora9<-cbind(VaRAE_95EMISORA9,VaRAE_975EMISORA9,VaRAE_99EMISORA9) 
+  colnames(VaRAE_emisora9) <-c("95%","97.5%","99%")  
+  VaRAE_emisora9
+  
+# ---------------------------------------------------------------------------------      
+  
+# ---------------------------------------------------------------------------------
+  # LABB
+  Tabla_Alisado10<-Tabla_Alisado[,c(10,11)]
+  Tabla_Alisado10 <- Tabla_Alisado10[with(Tabla_Alisado10, order(-Tabla_Alisado10$LABB.MX)), ] 
+  
+  # Creamos la columna de Fx
+  Alisado10<-Tabla_Alisado10$Alisado
+  
+  Fx<-vector()
+  
+  for(i in 1:length(Alisado10)){
+    Fx[i]<-sum(Alisado10[i:length(Alisado10)])
+  }
+  
+  head(Fx,30)
+  
+  # Unimos la columna de Fx con la Tabla_Alisado
+  Tabla_Alisado10<-cbind(Tabla_Alisado10,Fx)
+  
+  # VaR al 95% (1 día)
+  tabla_emisora10_95<-Tabla_Alisado10[which(Tabla_Alisado10$Fx>=0.95),] # Guarda todos los valores mayores a 0.95
+  VaRAE_95EMISORA10<-tabla_emisora10_95[length(tabla_emisora10_95$Fx),1] # Se toma el primer valor que sea mayor a 0.95
+  
+  # VaR al 975% (1 día)
+  tabla_emisora10_975<-Tabla_Alisado10[which(Tabla_Alisado10$Fx>=0.975),]
+  VaRAE_975EMISORA10<-tabla_emisora10_975[length(tabla_emisora10_975$Fx),1]
+  
+  # VaR al 99% (1 día)
+  tabla_emisora10_99<-Tabla_Alisado10[which(Tabla_Alisado10$Fx>=0.99),]
+  VaRAE_99EMISORA10<-tabla_emisora10_99[length(tabla_emisora10_99$Fx),1]
+  
+  # Resultados - VaR 1 dia de WalMex  
+  VaRAE_emisora10<-cbind(VaRAE_95EMISORA10,VaRAE_975EMISORA10,VaRAE_99EMISORA10) 
+  colnames(VaRAE_emisora10) <-c("95%","97.5%","99%")  
+  VaRAE_emisora10
+  
+# ---------------------------------------------------------------------------------      
+  
+
+# Metemos todos los valores encontrados del VaR - 1 dia en un dataframe
+  
+  VaR_AE1<-data.frame()
+  
+    for (j in 1:3){
+      VaR_AE1[1,j]<-VaRAE_emisora1[j]
+      VaR_AE1[2,j]<-VaRAE_emisora2[j]
+      VaR_AE1[3,j]<-VaRAE_emisora3[j]
+      VaR_AE1[4,j]<-VaRAE_emisora4[j]
+      VaR_AE1[5,j]<-VaRAE_emisora5[j]
+      VaR_AE1[6,j]<-VaRAE_emisora6[j]
+      VaR_AE1[7,j]<-VaRAE_emisora7[j]
+      VaR_AE1[8,j]<-VaRAE_emisora8[j]
+      VaR_AE1[9,j]<-VaRAE_emisora9[j]
+      VaR_AE1[10,j]<-VaRAE_emisora10[j]
+    }
+colnames(VaR_AE1)<-c("95%","97.5%","99%") # Le damos nombres a las columnas
+rownames(VaR_AE1)<-cartera # Le damos nombre a las filas
+View(VaR_AE1)
+
+# -------------------------------------------------------------------------------
+# Resultados
+
+# Hacemos la suma por columnas para obtener el VaR No Diversificado - 1 dia
+VaR_AE1["VaR No Diversificado",]<-colSums(VaR_AE1)
+View(VaR_AE1)
+
+
+# Obtenemos el VaR No Diversificado 30 - dias
+VaR_AE30<-VaR_AE1*30
+View(VaR_AE30)
+
+# Obtenemos el VaR No Diversificado 180 - dias
+VaR_AE180<-VaR_AE1*180
+View(VaR_AE180)
+
+# Obtenemos el VaR No Diversificado 360 - dias
+VaR_AE360<-VaR_AE1*360
+View(VaR_AE360)
+# -------------------------------------------------------------------------------
+
+  
+  
+    
